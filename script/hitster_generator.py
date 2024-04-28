77b1ff1dded267e1d6cad92c52375190d06a345f
Aitor <aitor@MacBook-Pro-de-Aitor.local>
2024-04-28 12:38:39 +0200

    Añadida funcionalidad de dos modos: modo playlist o modo excel

--- script/hitster_generator.py
+++ script/hitster_generator.py
@@ -1,3 +1,8 @@
+
+###############################
+### LIBRERÍAS A UTILIZAR ######
+###############################
+
 import qrcode
 from PIL import Image
 import os
@@ -10,30 +15,42 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
+
 ################################################
 ### DATOS A INCLUIR POR PARTE DEL USUARIO ######
 ################################################
 
-# Ruta del PowerPoint final
+# ¿EN QUÉ MODO QUIERES UTILIZAR EL SCRIPT?
+modo_de_uso = 'desde_playlist' # 'desde_excel' o 'desde_playlist'
+
+# SEGÚN MODO, ELIGE EL INPUT DE DATOS
+playlist_url = 'https://open.spotify.com/playlist/5TOMoP3KEWtRASGeFsksUY?si=fbb64fcd02d5468a'
+excel_url = '/Users/aitor/Desktop/hitster/modo_excel/import_en_excel.xlsx'
+
+# Ruta para guardar los archivos finales de PPT y EXCEL
 pptx_path = "/Users/aitor/Desktop/hitster/archivos_generados/tarjetas_a_imprimir.pptx"
+xlsx_path = "/Users/aitor/Desktop/hitster/archivos_generados/listado_canciones.xlsx"
 
 # Datos personales del usuario para el uso de Spotify
 client_id = 'b0ce861df37447f5965272f0f620c964'
 client_secret = 'be5e4971b3cf4cfe8bb0b585e77684bd'
 
-# URL de la playlist a convertir
-playlist_url = 'https://open.spotify.com/playlist/5TOMoP3KEWtRASGeFsksUY?si=fbb64fcd02d5468a'
 
 
+################################################
+### AUTHENTICACIÓN PARA LA API DE SPOTIFY ######
+################################################
 
-##################
-### SCRIPT ######
-##################
-
+# La autenticación hay que hacerla para cualquiera de los dos modos (o desde playlist o desde Excel)
 auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
+###################################
+### FUNCIONES PARA EL SCRIPT ######
+###################################
+
+# FUNCIÓN A: para obtener datos de canciones a partir de una URL de una lista de reproducción de Spotify
 def obtener_info_playlist(url):
     playlist_id = url.split("/")[-1].split("?")[0]
     results = sp.playlist_tracks(playlist_id)
@@ -55,23 +72,84 @@ def obtener_info_playlist(url):
         
         canciones.append({
             'URL': url_cancion,
-            'Título': titulo,
-            'Artistas': artistas,
-            'Año de publicación': año_publicacion,
-            'Posición': posicion,  # Posición actual de la canción
-            'Proporción': proporcion  # Formato 'POSICION / TOTAL CANCIONES' como string
+            'Titulo': titulo,
+            'Artista': artistas,
+            'Año': año_publicacion,
+            'Pos': posicion,  # Posición actual de la canción
+            'PosRel': proporcion  # Formato 'POSICION / TOTAL CANCIONES' como string
         })
         posicion += 1  # Actualizar posición para la próxima canción
     
     return canciones
 
 
-# Usa la función con una URL de una lista de reproducción de Spotify
-info_canciones = obtener_info_playlist(playlist_url)
+# FUNCIÓN B: para obtener datos de canciones a partir de un archivo de Excel con url de canciones de Spotify
+def obtener_info_canciones_desde_excel(ruta_archivo):
+    # Cargar datos del archivo Excel
+    df_urls = pd.read_excel(ruta_archivo)
+    
+    # Configurar credenciales para la API de Spotify
+    client_id = 'tu_client_id'
+    client_secret = 'tu_client_secret'
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    
+    # Lista para almacenar los datos de las canciones
+    canciones_info = []
+    
+    # Número total de canciones
+    total_canciones = len(df_urls)
+    
+    # Iterar sobre las URLs en el DataFrame, utilizando enumerate para obtener la posición
+    for posicion, url in enumerate(df_urls['URL'], start=1):
+        track_id = url.split("/")[-1]
+        try:
+            # Obtener datos de la canción
+            track = sp.track(track_id)
+            track_name = track['name']
+            track_url = track['external_urls']['spotify']
+            release_year = track['album']['release_date'][:4]
+            artist_names = ', '.join([artist['name'] for artist in track['artists']])
+            proporcion = f'{posicion} / {total_canciones}'
+            
+            # Añadir la información a la lista
+            canciones_info.append({
+                'URL': track_url,
+                'Titulo': track_name,
+                'Artista': artist_names,
+                'Año': release_year,
+                'Pos': posicion,
+                'PosRel': proporcion
+            })
+        except Exception as e:
+            print(f"No se pudo obtener información para la URL: {url}. Error: {str(e)}")
+    
+    # Crear un DataFrame con la información recolectada
+    df_resultado = pd.DataFrame(canciones_info)
+    
+    return df_resultado
 
-df = pd.DataFrame(info_canciones)
-df.rename(columns={'Artistas': 'Artista', 'Año de publicación': 'Año', 'Posición': 'Pos'}, inplace=True)
-print(df)
+
+###########################################################################################
+### APLICACIÓN DE UNO DE LOS DOS MODOS PARA OBTENER LOS DATOS DE LAS CANCIONES EN DF ######
+###########################################################################################
+
+
+if modo_de_uso == 'desde_excel': 
+    info_canciones = obtener_info_canciones_desde_excel(excel_url)
+    print(info_canciones)
+elif modo_de_uso == 'desde_playlist':
+    info_canciones_lista = obtener_info_playlist(playlist_url)
+    info_canciones = pd.DataFrame(info_canciones_lista)
+    print(info_canciones)
+else:
+    print("Error")
+
+
+
+#####################################################################################################
+### CREACIÓN DE QRS, Y CONSTRUCCIÓN Y MAQUETACIÓN DEL PPT FINAL CON LOS DATOS DE LAS CANCIONES ######
+#####################################################################################################
 
 # Crear una presentación de PowerPoint
 prs = Presentation()
@@ -91,10 +169,10 @@ space_after = Pt(12)
 text_box_left = (prs.slide_width - text_box_width) / 2
 text_box_top = (prs.slide_height - qr_size) / 2  # Asumiendo que queremos centrar respecto al QR
 
-for index, row in df.iterrows():
+for index, row in info_canciones.iterrows():
     # Datos de cada fila
     url = row['URL']
-    title = row['Título']
+    title = row['Titulo']
     artist = row['Artista']
     year = row['Año']
 
@@ -155,8 +233,13 @@ for index, row in df.iterrows():
     p.alignment = PP_ALIGN.CENTER
 
 
+#########################################
+### EXPORTACIÓN DE LOS RESULTADOS ######
+#########################################
+
 # Export DataFrame to an Excel file
-df.to_excel(pptx_path.replace('.pptx', '.xlsx'), index=False)
+info_canciones.to_excel(xlsx_path, index=False)
+
 # Guardar la presentación
 prs.save(pptx_path)
 print(f"PowerPoint guardado en {pptx_path} con éxito güey!")
